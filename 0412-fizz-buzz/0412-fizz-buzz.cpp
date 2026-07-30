a47c9b1c34b90f6fa323c50312ee8d14ddd60fe8
Ashraf <mohammadashrafullah001@gmail.com>
2026-07-30 16:07:46 +0530

    Time: 0 ms (100%), Space: 11.1 MB (94.68%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>res(n);
+        for(int i=0;i<n;i++){
+            int val=i+1;
+            if(val%3==0&&val%5==0) res[i]="FizzBuzz";
+            else if(val%3==0) res[i]="Fizz";
+            else if(val%5==0) res[i]="Buzz";
+            else res[i]=to_string(i+1);
+        }
+        return res;
+    }
+};
