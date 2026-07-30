5d52e9874d8a5f5912016035624ee509e5c0e0dc
Ashraf <mohammadashrafullah001@gmail.com>
2026-07-30 16:12:50 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.97%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int result=0;
+        int balance=0;
+        for(char c:s){
+            if(c=='L') balance++;
+            else balance--;
+            if(balance==0) result++;
+        }
+        return result;
+    }
+};
