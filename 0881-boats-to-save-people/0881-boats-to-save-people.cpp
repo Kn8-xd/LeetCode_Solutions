8966c4e8e466d4eb70b46ce26764fb4b7cab7cf8
Ashraf <mohammadashrafullah001@gmail.com>
2026-08-05 20:59:57 +0530

    Time: 19 ms (47.97%), Space: 45.8 MB (39.63%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n=people.size();
+        int l=0,r=n-1,sol=0;
+        sort(people.begin(),people.end());
+        while(l<=r) {
+            if(people[l]+people[r]<=limit) {
+                l++;
+                r--;
+                sol++;
+            }
+            else {
+                r--;
+                sol++;
+            }
+        }
+        return sol;
+    }
+};
