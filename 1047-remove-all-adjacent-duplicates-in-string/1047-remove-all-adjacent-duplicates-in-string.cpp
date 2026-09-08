71e9106046a5d0fb6e5e8a7224db15f39df28051
Ashraf <mohammadashrafullah001@gmail.com>
2026-09-08 14:54:27 +0530

    Time: 7 ms (51.07%), Space: 14.4 MB (18.3%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>stk;
+        string str="";
+        for(char c:s) {
+            if(!stk.empty()&&stk.top()==c) {
+                stk.pop();
+                str.pop_back();
+            } else {
+                stk.push(c);
+                str.push_back(c);
+            }
+        }
+        return str;
+    }
+};
